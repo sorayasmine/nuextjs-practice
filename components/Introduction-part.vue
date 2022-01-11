8efa504b8265ac5afca60b4d80e0a9a70714ac87
tiara <niceefforts42@gmail.com>
2022-01-11 08:58:52 +0700

    add files of nuxt js

---
+++ components/Introduction-part.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-80 text-center items-center justify-center">
+    <h2 class="text-2xl text-red-500 font-medium">
+      Halo, perkenalkan nama saya Tiara Yasmine Soraya
+    </h2>
+    <h3 class="text-xl text-green-500">
+      Motivasi saya belajar di JCC ialah untuk memperdalam ilmu di bidang Front
+      End development khususnya dengan menggunakan Vue JS serta mendapatkan
+      kesempatan kerja yang lebih baik
+    </h3>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <Introduction-part />
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
